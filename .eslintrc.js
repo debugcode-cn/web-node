f946d371828d0a3debb0215c82c3769f0028b99f
wanglei <lei.wang@keymantech.cn>
2022-08-23 22:10:37 +0800

    change indexnt_style

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,15 @@
 module.exports = {
-	env: {
-		browser: true,
-		commonjs: true,
-		es2021: true,
-	},
-	extends: 'eslint:recommended',
-	parserOptions: {
-		ecmaVersion: 'latest',
-	},
-	rules: {
-		'no-unused-vars': 'off',
-		'no-undef': 0,
-	},
+    env: {
+        browser: true,
+        commonjs: true,
+        es2021: true,
+    },
+    extends: 'eslint:recommended',
+    parserOptions: {
+        ecmaVersion: 'latest',
+    },
+    rules: {
+        'no-unused-vars': 'off',
+        'no-undef': 0,
+    },
 };
