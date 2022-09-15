7ba26e33867cc1383ba05368282225de2335ab53
wanglei <lei.wang@keymantech.cn>
2022-09-16 03:44:55 +0800

    查询OK

--- src/components/elasticsearch/index.js
+++ src/components/elasticsearch/index.js
@@ -1,47 +1,51 @@
 'use strict';
 
 const { Client } = require('@elastic/elasticsearch');
-const client = new Client({ node: 'http://192.168.3.25:9200' });
+const client = new Client({
+    node: 'http://192.168.3.25:9210'
+});
 
 async function run() {
     // Let's start by indexing some data
-    await client.index({
-        index: 'game-of-thrones',
-        document: {
-            character: 'Ned Stark',
-            quote: 'Winter is coming.',
-        },
-    });
+    // await client.index({
+    //     index: 'game-of-thrones',
+    //     document: {
+    //         character: 'Ned Stark',
+    //         quote: 'Winter is coming.',
+    //     },
+    // });
 
-    await client.index({
-        index: 'game-of-thrones',
-        document: {
-            character: 'Daenerys Targaryen',
-            quote: 'I am the blood of the dragon.',
-        },
-    });
+    // await client.index({
+    //     index: 'game-of-thrones',
+    //     document: {
+    //         character: 'Daenerys Targaryen',
+    //         quote: 'I am the blood of the dragon.',
+    //     },
+    // });
 
-    await client.index({
-        index: 'game-of-thrones',
-        document: {
-            character: 'Tyrion Lannister',
-            quote: 'A mind needs books like a sword needs a whetstone.',
-        },
-    });
+    // await client.index({
+    //     index: 'game-of-thrones',
+    //     document: {
+    //         character: 'Tyrion Lannister',
+    //         quote: 'A mind needs books like a sword needs a whetstone.',
+    //     },
+    // });
 
-    // here we are forcing an index refresh, otherwise we will not
-    // get any result in the consequent search
-    await client.indices.refresh({ index: 'game-of-thrones' });
+    // // here we are forcing an index refresh, otherwise we will not
+    // // get any result in the consequent search
+    // await client.indices.refresh({ index: 'game-of-thrones' });
 
     // Let's search!
     const result = await client.search({
         index: 'game-of-thrones',
         query: {
-            match: { quote: '*' },
+            match: {
+                character: "Daenerys*"
+            },
         },
     });
 
-    console.log(result.hits.hits);
+    console.log(result.hits);
 }
 
 run().catch(console.log);
